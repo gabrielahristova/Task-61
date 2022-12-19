e28adb445cd8808c7a4bc177b9bd1dd1c7a18439
Gabriela Hristova <gabriela.hr1406@gmail.com>
2022-12-19 17:51:21 +0200

    add fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let priceElements = document.querySelectorAll('.price');
+  for(var i=0; i<=priceElements.length; i++){
+    if(priceElements[i].classList.contains("hot")){
+      priceElements[i].innerHTML+= '🔥';
+    }
+  }
 });
