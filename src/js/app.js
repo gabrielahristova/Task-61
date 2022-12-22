9477fd01187ed01a4228c468272f9b5f297e66f9
Gabriela Hristova <gabriela.hr1406@gmail.com>
2022-12-22 14:16:10 +0200

    repair

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,10 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   let priceElements = document.querySelectorAll('.price');
-  for(var i=0; i<=priceElements.length; i++){
-    if(priceElements[i].classList.contains("hot")){
-      priceElements[i].innerHTML+= '🔥';
-    }
+  console.log(priceElements);
+priceElements.forEach(element => {
+  if(element.classList.contains('hot')) {
+    element.innerHTML += '🔥';
   }
+});
 });
